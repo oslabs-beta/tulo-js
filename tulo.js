a5a247fe35fb1812bbf07e9e01cbff3b712abcb8
abitshaken <30978672+abitshaken@users.noreply.github.com>
2021-07-12 11:58:31 -0500

    Added metrics queue to batch messages to Tulu App with support for offline handling

--- tulo.js
+++ tulo.js
@@ -1,9 +1,17 @@
+const METRICS_BATCH_SIZE = 10;
 export const cacheGenerator = (cacheSpecs) => {
-  const version = 1.0;
-
+  let metricsQueue = [];
   const sendMetrics = (metrics) => {
-    //sends to server
     console.log(metrics?.message);
+    if (navigator.onLine && metricsQueue.length >= METRICS_BATCH_SIZE) {
+      //sends to server
+      //flush queue if online
+      console.log('Sending to Server and Flushing Metrics Queue');
+      metricsQueue = [];
+    } else {
+      metricsQueue.push(metrics);
+      console.log('Adding to Metrics Queue. Current Queue:', metricsQueue);
+    }
   };
 
   const setUpCache = () => {
@@ -43,52 +51,82 @@ export const cacheGenerator = (cacheSpecs) => {
     e.waitUntil(deleteOldCaches().then(() => clients.claim()));
   });
 
-  const getSpec = request => {
-    for(const spec of cacheSpecs){
+  const getSpec = (request) => {
+    for (const spec of cacheSpecs) {
       const types = spec.types;
-      for(let type of types){
-        console.log('Type',type);
-        if (request.headers.get('Accept').includes(type)){
+      for (let type of types) {
+        if (request.headers.get('Accept').includes(type)) {
           return spec;
         }
       }
     }
-  }
+  };
 
-  const cacheFirst = async (request, cacheName) => {
-    try{
+  const cacheFirst = async (e, cacheName) => {
+    try {
+      const { request } = e;
       const cache = await caches.open(cacheName);
-      let res = await cache.match(request);
-      if(res){
-        sendMetrics({message: 'Found in Cache'});
-      } else{
-        sendMetrics({message: 'Not Found in Cache'});
-        res = await fetch(request);
-        if(res){
-          sendMetrics({message: 'Found in Network'});
-        } else{
-          sendMetrics({message: 'Not Found in Network'});
-        }
+      let response = await cache.match(request);
+      if (response) {
+        sendMetrics({ message: 'Found in Cache' });
+        return response;
       }
-      return res;
-    } catch(err){
-      console.error('Somewthing went wrong');
-    }
-  }
 
-  const runStrategy = (request) => {
-    const spec = getSpec(request);
-    console.log(spec);
-    const { strategy } = spec;
-    if(strategy === 'CacheFirst'){
-      return cacheFirst(request, spec.name);
+      sendMetrics({ message: 'Not Found in Cache' });
+      response = await fetch(request);
+      if (response) {
+        sendMetrics({ message: 'Found in Network' });
+        let copy = response.clone();
+        e.waitUntil(
+          cache.put(request, copy)
+        )
+        sendMetrics({ message: 'Added to Cache' });
+        return response;
+      }
+      
+      sendMetrics({ message: 'Not Found in Network' });
+      return new Response();
+    } catch (err) {
+      console.error('Somewthing went wrong', err);
     }
-  }
-  self.addEventListener('fetch', (e) => {
+  };
+
+  const networkFirst = async (e, cacheName) => {
+    try {
+      const { request } = e;
+      const cache = await caches.open(cacheName);
+      let response = await fetch(request);
+      if (response) {
+        sendMetrics({ message: 'Found in Network' });
+        let copy = response.clone();
+        e.waitUntil(
+          cache.put(request, copy)
+        )
+        sendMetrics({ message: 'Added to Cache' });
+        return response;
+      }
+      
+      sendMetrics({ message: 'Not Found in Network' });
+      return new Response();
+    } catch (err) {
+      console.error('Somewthing went wrong', err);
+    }
+  };
+
+  const runStrategy = async (e) => {
     const { request } = e;
+    const spec = getSpec(request);
+    if (!spec) return await fetch(request);
+    const { strategy } = spec;
+    switch(strategy){
+      case 'CacheFirst': return cacheFirst(e, spec.name)
+      case 'NetworkFirst': return networkFirst(e, spec.name)
+      default: return new Response()
+    };
+  };
+
+  self.addEventListener('fetch', (e) => {
     console.log('Service Worker Fetching...');
-    e.respondWith(
-      runStrategy(request)
-    )
+    e.respondWith(runStrategy(e));
   });
 };
